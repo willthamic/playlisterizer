46ed2e1420784a8bf68845f17cd44f047441dccb
willthamic <will.hamic@gmail.com>
2020-04-18 02:53:41 -0500

    fix

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-location.href = 
-    "https://accounts.spotify.com/authorize" +
-    "?client_id=c298d6f931fc45db88dee8d2c14183bc" +
-    "&redirect_uri=C%3A%5CDrive%5CCode%5Cplaylisterizer%5Cindex.html" + 
-    "&scope=user-read-private%20user-read-email" + 
-    "&response_type=token" + 
-    "&state=123";
+function authenticate() {
+    var client_id = "c298d6f931fc45db88dee8d2c14183bc";
+    var redirect_uri = "http%3A%2F%2Fwillhamic.com%2Fplaylisterizer%2Findex.html";
+    var scope = [
+        "user-read-private",
+         "user-read-email"
+    ].join("%20");
+
+    location.href = "https://accounts.spotify.com/authorize?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&scope=" + scope + "&response_type=token";
+}
 
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken('<here_your_access_token>');
